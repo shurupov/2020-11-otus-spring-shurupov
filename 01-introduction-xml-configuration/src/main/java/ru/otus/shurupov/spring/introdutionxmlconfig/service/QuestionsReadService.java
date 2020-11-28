639de8eabedd519cfcff6e1d04307716eb1dd5c5
Evgeny Shurupov <bolteg86@ya.ru>
2020-11-29 00:13:25 +0400

    01 Added packaging to JAR

--- 01-introduction-xml-configuration/src/main/java/ru/otus/shurupov/spring/introdutionxmlconfig/service/QuestionsReadService.java
+++ 01-introduction-xml-configuration/src/main/java/ru/otus/shurupov/spring/introdutionxmlconfig/service/QuestionsReadService.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 import ru.otus.shurupov.spring.introdutionxmlconfig.domain.Question;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,8 +21,8 @@ public class QuestionsReadService {
 
     public List<Question> readQuestions() throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource(questionsFileName).getFile());
-        Reader in = new FileReader(file);
+        InputStream csvStream = classLoader.getResource(questionsFileName).openStream();
+        Reader in = new InputStreamReader(csvStream);
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader(new String[]{"question", "answer1", "answer2", "answer3"}).parse(in);
         List<Question> questions = new ArrayList<>();
         for (CSVRecord record : records) {
