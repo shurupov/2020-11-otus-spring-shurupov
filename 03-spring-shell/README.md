f175441da9a8da2caf6e871c2e9d42aa41ebbbb0
Evgeny Shurupov <bolteg86@ya.ru>
2020-12-13 20:49:36 +0400

    05 Spring shell start

---
+++ 03-spring-shell/README.md
@@ -0,0 +1,8 @@
+# Spring Boot
+
+## Task
+Need to move the previous application to Spring Boot, implement internationalization, use application.yml, create load banner, use spring-boot-starter-test. 
+
+To build project to jar use command `maven package`
+
+To start application use command `java -jar target/03-spring-boot-0.0.1-SNAPSHOT.jar`
---
+++ 03-spring-shell/src/main/java/ru/otus/shurupov/spring/springboot/service/InteractiveServiceSystemImpl.java
@@ -0,0 +1,30 @@
+package ru.otus.shurupov.spring.springboot.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
+
+@Service
+public class InteractiveServiceSystemImpl implements InteractiveService {
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public String readString() {
+        return scanner.next();
+    }
+
+    @Override
+    public int readInt() {
+        return scanner.nextInt() - 1;
+    }
+
+    @Override
+    public void println(String text) {
+        System.out.println(text);
+    }
+
+    @Override
+    public void println() {
+        System.out.println();
+    }
+}
