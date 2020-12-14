c76bce6d536d03990d2448f61cedb16f1fefc845
Evgeny Shurupov <bolteg86@ya.ru>
2020-12-14 18:23:17 +0400

    05 Removed interface

--- 05-spring-shell/src/main/java/ru/otus/shurupov/spring/springboot/service/QuizEventListener.java
+++ 05-spring-shell/src/main/java/ru/otus/shurupov/spring/springboot/service/QuizEventListener.java
@@ -1,7 +1,18 @@
 package ru.otus.shurupov.spring.springboot.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
 import ru.otus.shurupov.spring.springboot.event.EnterFirstNameEvent;
 
-public interface QuizEventListener {
-    void setFirstName(EnterFirstNameEvent enterFirstNameEvent);
+@Service
+@RequiredArgsConstructor
+public class QuizEventListener {
+
+    private final QuizService quizService;
+
+    @EventListener
+    public void setFirstName(EnterFirstNameEvent enterFirstNameEvent) {
+        quizService.setFirstName(enterFirstNameEvent.getFirstName());
+    }
 }
