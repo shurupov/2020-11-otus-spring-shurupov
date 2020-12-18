2fe30a242483894b15163163741e7d0be039d73e
Evgeny Shurupov <bolteg86@ya.ru>
2020-12-18 14:28:19 +0400

    05 Added junit-jupiter to create mocks by @Mock annotation

--- 05-spring-shell/src/test/java/ru/otus/shurupov/spring/springshell/service/GreetingServiceImplTest.java
+++ 05-spring-shell/src/test/java/ru/otus/shurupov/spring/springshell/service/GreetingServiceImplTest.java
@@ -3,18 +3,22 @@ package ru.otus.shurupov.spring.springshell.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.*;
 
 @DisplayName("GreetingServiceImpl")
+@ExtendWith(MockitoExtension.class)
 class GreetingServiceImplTest {
 
+    @Mock
     private OutputService outputService;
     private GreetingService greetingService;
 
     @BeforeEach
     void setUp() {
-        outputService = mock(OutputService.class);
         greetingService = new GreetingServiceImpl(outputService);
     }
 
