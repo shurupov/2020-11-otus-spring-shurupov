5819f1d790111730ca04c4cd33fadefc02e2b1f6
Evgeny Shurupov <bolteg86@ya.ru>
2020-12-21 14:48:55 +0400

    07 Added yaml insert

--- 07-spring-jdbc/src/main/resources/db/changelog/1.0/data/2020-12-21--001-genres.sql
+++
@@ -1,9 +0,0 @@
-insert into genre (name)
-values ('Action and Adventure'),
-       ('Crime and Detective'),
-       ('Fairy Tale'),
-       ('Fantasy'),
-       ('Satire'),
-       ('Science Fiction'),
-       ('Poem'),
-       ('Drama');
