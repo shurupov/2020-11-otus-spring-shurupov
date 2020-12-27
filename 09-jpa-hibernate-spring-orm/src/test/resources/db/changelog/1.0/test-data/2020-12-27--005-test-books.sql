502b24c4bdc54cdbab636413d282230f26747519
Evgeny Shurupov <bolteg86@ya.ru>
2020-12-27 20:35:57 +0400

    09 Fixed liquibase changelogs to make it work both from scratch and from previous homework database

--- 09-jpa-hibernate-spring-orm/src/test/resources/db/changelog/1.0/test-data/2020-12-27--005-test-books.sql
+++
@@ -1,5 +0,0 @@
-insert into book (author_id, name) values
-    (1, 'Sherlock Holmes. A Study in Scarlet'),
-    (2, 'Hercule Poirot. The Mysterious Affair at Styles'),
-    (3, 'The Tale about a Fisherman and a Fish'),
-    (4, 'Crime and Punishment');
