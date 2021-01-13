fd8e4334fa94aeb9552c62526d830dab96edc93c
Evgeny Shurupov <bolteg86@ya.ru>
2021-01-13 17:18:23 +0400

    11 Started

---
+++ 11-spring-data-jpa/src/main/java/ru/otus/shurupov/spring/jpa/dao/BookDao.java
@@ -0,0 +1,15 @@
+package ru.otus.shurupov.spring.jpa.dao;
+
+import ru.otus.shurupov.spring.jpa.domain.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookDao {
+    long count();
+    Optional<Book> getById(Long id);
+    void insert(Book book);
+    List<Book> getAll();
+    void remove(Book book);
+    void updateNameById(long id, String name);
+}
---
+++ 11-spring-data-jpa/src/main/java/ru/otus/shurupov/spring/jpa/service/GenreService.java
@@ -0,0 +1,18 @@
+package ru.otus.shurupov.spring.jpa.service;
+
+import ru.otus.shurupov.spring.jpa.domain.Genre;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GenreService {
+    long count();
+    Optional<Genre> getById(Long id);
+    void insert(String name);
+    List<Genre> getAll();
+
+    void displayList();
+    void displayById(Long id);
+
+    String getGenreCaption(List<Genre> genres);
+}
