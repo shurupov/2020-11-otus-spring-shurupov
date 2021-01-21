b00a14e34375299a27ae9db1c0481df31845424b
Evgeny Shurupov <bolteg86@ya.ru>
2021-01-22 00:03:09 +0400

    13 Added test with embedded mongo

--- 13-spring-data-mongodb/src/main/java/ru/otus/shurupov/spring/springdatamongodb/domain/Author.java
+++ 13-spring-data-mongodb/src/main/java/ru/otus/shurupov/spring/springdatamongodb/domain/Author.java
@@ -1,10 +1,14 @@
 package ru.otus.shurupov.spring.springdatamongodb.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "author")
 public class Author {
     @Id
--- 13-spring-data-mongodb/src/main/java/ru/otus/shurupov/spring/springdatamongodb/domain/Book.java
+++ 13-spring-data-mongodb/src/main/java/ru/otus/shurupov/spring/springdatamongodb/domain/Book.java
@@ -1,6 +1,7 @@
 package ru.otus.shurupov.spring.springdatamongodb.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 @Document(collection = "book")
 public class Book {
 
@@ -18,4 +20,10 @@ public class Book {
     private Author author;
     private String name;
     private List<String> genres;
+
+    public Book(Author author, String name, List<String> genres) {
+        this.author = author;
+        this.name = name;
+        this.genres = genres;
+    }
 }
