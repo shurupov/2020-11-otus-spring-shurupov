cc15f4e2d836ed9c1d91097eccf55f107de1e21d
Evgeny Shurupov <bolteg86@ya.ru>
2021-02-04 21:39:24 +0400

    15 Index page changes

--- 15-spring-mvc-view/src/main/java/ru/otus/shurupov/spring/springdata/controller/IndexController.java
+++ 15-spring-mvc-view/src/main/java/ru/otus/shurupov/spring/springdata/controller/IndexController.java
@@ -5,16 +5,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.otus.shurupov.spring.springdata.service.BookService;
+import ru.otus.shurupov.spring.springdata.service.GenreService;
+
+import java.util.Collections;
 
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final BookService bookService;
+    private final GenreService genreService;
 
     @GetMapping("/")
     public String bookList(Model model) {
         model.addAttribute("books", bookService.count());
+        model.addAttribute("genres", genreService.count());
+        model.addAttribute("breadcrumbs", Collections.emptyList());
         return "index";
     }
 
