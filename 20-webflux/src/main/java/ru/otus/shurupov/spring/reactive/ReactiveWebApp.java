5c68b9e7a95e6b704d2a0730918f7d72254714f8
Evgeny Shurupov <bolteg86@ya.ru>
2021-02-25 19:52:26 +0400

    20 Webflux start

---
+++ 20-webflux/src/main/java/ru/otus/shurupov/spring/reactive/ReactiveWebApp.java
@@ -0,0 +1,11 @@
+package ru.otus.shurupov.spring.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveWebApp.class);
+    }
+}
