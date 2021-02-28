51d20f28a3f6af658d1bf20892e75f027a0d4727
Evgeny Shurupov <bolteg86@ya.ru>
2021-02-28 14:56:24 +0400

    20 Added endpoint

--- 20-webflux/src/main/java/ru/otus/shurupov/spring/reactive/ReactiveWebApp.java
+++ 20-webflux/src/main/java/ru/otus/shurupov/spring/reactive/ReactiveWebApp.java
@@ -1,21 +1,21 @@
 package ru.otus.shurupov.spring.reactive;
 
 import com.github.cloudyrock.spring.v5.EnableMongock;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.otus.shurupov.spring.reactive.domain.Book;
+import ru.otus.shurupov.spring.reactive.domain.dto.BookDto;
+import ru.otus.shurupov.spring.reactive.repository.AuthorRepository;
 import ru.otus.shurupov.spring.reactive.repository.BookRepository;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.BodyInserters.fromValue;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
-import static org.springframework.web.reactive.function.server.RequestPredicates.queryParam;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
@@ -28,7 +28,7 @@ public class ReactiveWebApp {
     }
 
     @Bean
-    public RouterFunction<ServerResponse> bookRoutes(BookRepository bookRepository) {
+    public RouterFunction<ServerResponse> bookRoutes(BookRepository bookRepository, AuthorRepository authorRepository) {
         return route()
                 .GET(
                         "/api/2/books",
@@ -39,6 +39,21 @@ public class ReactiveWebApp {
                         request -> bookRepository.findById(request.pathVariable("id"))
                             .flatMap(book -> ok().body(fromValue(book)))
                     )
+                .POST(
+                        "/api/2/books",
+                        accept(APPLICATION_JSON),
+                        request -> request.bodyToMono(BookDto.class)
+                                .flatMap(book -> authorRepository.findById(book.getAuthorId())
+                                        .flatMap(author -> {
+                                            Book book1 = new Book();
+                                            book1.setAuthor(author);
+                                            book1.setName(book.getName());
+                                            book1.setGenres(book.getGenres());
+                                            book1.setComments(book.getComments());
+                                            return Mono.just(book1);
+                                        }).flatMap(bookRepository::save))
+                                .flatMap(book -> ok().body(fromValue(book)))
+                )
                 .build();
     }
 }
