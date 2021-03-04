71dfd94cee1f1eb4d028764c31868638d95e4c79
Evgeny Shurupov <bolteg86@ya.ru>
2021-03-05 00:06:15 +0400

    23 Implemented own SPA Response Login Filter and Authentication Success Handler

--- 23-security-authentication/src/main/java/ru/otus/shurupov/spring/authentication/config/SecurityConfiguration.java
+++ 23-security-authentication/src/main/java/ru/otus/shurupov/spring/authentication/config/SecurityConfiguration.java
@@ -1,12 +1,19 @@
 package ru.otus.shurupov.spring.authentication.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.ui.DefaultLoginPageGeneratingFilter;
+import ru.otus.shurupov.spring.authentication.config.security.SpaAuthenticationSuccessHandler;
+import ru.otus.shurupov.spring.authentication.config.security.SpaLoginResponseGeneratingFilter;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -16,12 +23,35 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/**").permitAll();
+                .authorizeRequests().antMatchers("/api/**").authenticated()
+                .and()
+                .authorizeRequests().mvcMatchers("/**").permitAll()
+                .and()
+                .addFilterBefore(new SpaLoginResponseGeneratingFilter(), DefaultLoginPageGeneratingFilter.class)
+                .formLogin()
+                    .loginProcessingUrl("/api/login")
+                    .successHandler(new SpaAuthenticationSuccessHandler())
+                ;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("admin").password("admin").roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return rawPassword.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return rawPassword.toString().equals(encodedPassword);
+            }
+        };
     }
 }
