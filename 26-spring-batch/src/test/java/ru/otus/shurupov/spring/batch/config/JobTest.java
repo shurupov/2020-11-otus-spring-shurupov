a17782692670d4e26567b7a55ebd89dc82392a16
Evgeny Shurupov <bolteg86@ya.ru>
2021-03-14 13:30:03 +0400

    26 Started test

---
+++ 26-spring-batch/src/test/java/ru/otus/shurupov/spring/batch/config/JobTest.java
@@ -0,0 +1,45 @@
+package ru.otus.shurupov.spring.batch.config;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.otus.shurupov.spring.batch.config.JobConfig.IMPORT_FROM_MONGO_TO_POSTGRES;
+
+@SpringBootTest
+@SpringBatchTest
+class JobTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+    @BeforeEach
+    void clearMetaData() {
+        jobRepositoryTestUtils.removeJobExecutions();
+    }
+
+    @Test
+    void testJob() throws Exception {
+
+        Job job = jobLauncherTestUtils.getJob();
+        assertThat(job).isNotNull()
+                .extracting(Job::getName)
+                .isEqualTo(IMPORT_FROM_MONGO_TO_POSTGRES);
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(new JobParameters());
+
+        assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo("COMPLETED");
+    }
+}
