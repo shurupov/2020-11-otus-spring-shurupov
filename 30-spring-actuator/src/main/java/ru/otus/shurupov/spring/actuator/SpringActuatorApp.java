9b88c0d1d27200820aab2c0995160c2451b691b9
Evgeny Shurupov <bolteg86@ya.ru>
2021-03-30 19:47:17 +0400

    30 First log settings

--- 30-spring-actuator/src/main/java/ru/otus/shurupov/spring/actuator/SpringActuatorApp.java
+++ 30-spring-actuator/src/main/java/ru/otus/shurupov/spring/actuator/SpringActuatorApp.java
@@ -1,5 +1,7 @@
 package ru.otus.shurupov.spring.actuator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,7 +11,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableSwagger2
 public class SpringActuatorApp {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpringActuatorApp.class);
+
     public static void main(String[] args) {
+        logger.info("Starting");
         SpringApplication.run(SpringActuatorApp.class);
+        logger.info("Started");
     }
 }
